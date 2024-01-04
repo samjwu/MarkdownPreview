28b3ba7a915cb245f487f6de9a32c9788b076c96
samjwu <sjwu@ualberta.ca>
2024-01-03 19:19:54 -0700

    Add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-export default class App extends React.Component{
-render(){
-  return (
-    <div className="App">
-     
-    </div>
-  );}
+import Badge from "react-bootstrap/Badge";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="text-align-center" variant="light">
+                  Markdown Preview
+                </Badge>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
